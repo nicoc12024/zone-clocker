97e533c9f69aedbd54e1e68968100253317fd79c
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2024-03-07 18:44:53 +0000

    Deleted unnecessary files except the client folder

--- api/controllers/auth/utils/token.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (userId, userType) => {
-  const payload = {
-    id: userId,
-    userType: userType,
-  };
-  return jwt.sign(payload, process.env.SECRET_KEY);
-};
