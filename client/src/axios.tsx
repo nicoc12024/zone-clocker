aa11cf00dec72bf3473c0a0bd881555c34d43a43
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2024-03-07 18:52:13 +0000

    :

--- client/src/axios.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
